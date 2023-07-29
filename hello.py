e0b424608555eec2f2815b4d9cbc325ec5001e51
Iago Silva da Cruz <iagosilvacontato@gmail.com>
2023-07-28 23:07:27 -0300

    comment about code works

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+O programa irá ler a linguagem configurada no ambiente
+e irá exibir um 'Hello World' no idioma correspondente
+"""
+
 # Ler mensagem de boas-vindas
 print('hello, World')

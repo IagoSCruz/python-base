9ce87994fab2a49e75dc0f2245371e604b99f481
Iago Silva da Cruz <iagosilvacontato@gmail.com>
2023-07-31 19:45:37 -0300

    Metadados do programa

--- hello.py
+++ hello.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 
-"""
-O programa irá ler a linguagem configurada no ambiente
+"""O programa irá ler a linguagem configurada no ambiente
 e irá exibir um 'Hello World' no idioma correspondente
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada. Ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
 """
+__version__ = "0.0.1"     # Dunder Version
+__author__ = "Iago Cruz"  # Dunder Author
+__license__ = "unlicense" # Dunder License
+
 
 # Ler mensagem de boas-vindas
 print('hello, World')
+
+

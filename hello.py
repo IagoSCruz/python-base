e6b5604599cc4f2c668961ef10400dd42134bbb9
Iago Silva da Cruz <iagosilvacontato@gmail.com>
2023-08-01 14:01:26 -0300

    Read Var Env

--- hello.py
+++ hello.py
@@ -19,8 +19,18 @@ __version__ = "0.0.1"     # Dunder Version
 __author__ = "Iago Cruz"  # Dunder Author
 __license__ = "unlicense" # Dunder License
 
+import os     # Ler variáveis do ambiente
 
-# Ler mensagem de boas-vindas
-print('hello, World')
+current_language = os.getenv("LANG")[:5]
+
+msg = 'Hello, World'
+
+if current_language == 'pt_BR':
+    msg = 'Olá, Mundo!'
+elif current_language == 'it_IT':
+    msg = 'Ciao, Mondo!'
+
+
+print(msg)
 
 

ff509ac3deb9d15867bb993f957b7146ebcc5108
Iago Cruz <iagosilvacontato@gmail.com>
2025-02-10 13:18:46 -0300

    refatorar prefixcal.py

--- prefixcalc.py
+++ prefixcalc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Calculadora Prefix
+"""Calculadora Prefix.
 
 Funcionamento:
 
@@ -12,44 +12,62 @@ mul -> *
 div -> /
 
 Uso:
-$ prefixcalc.py mul 10 5
->> 50
+$ prefixcalc.py sum 5 2
+7
 
-prefixcalc.py
+$ prefixcalc.py mul 10 5
+50
+
+$ prefixcalc.py
 operação: sum
 n1: 5
 n2: 4
 9
 """
-__version__ = '0.1.0'
-
+__version__ = "0.1.0"
 
 import sys
-argumentos = sys.argv[1:]
 
+arguments = sys.argv[1:]
 
-if not argumentos:
-    operacao = (input('Operação: '))
-    n1 = int(input('Informe um número: '))
-    n2 = int(input('Informe outro número: '))
-    argumentos = [operacao, n1, n2]
-
-elif len(argumentos) != 3:
-    print('Argumentos inválidos!')
-    print()
-    print('Ex: `sum 5 5`')
+if not arguments:
+    operation = input("operação:")
+    n1 = input("n1:")
+    n2 = input("n2:")
+    arguments = [operation, n1, n2]
+elif len(arguments) != 3:
+    print("Número de argumentos inválidos")
+    print("ex: `sum 5 5`")
     sys.exit(1)
 
-operacao, *numeros = argumentos
+operation, *nums = arguments
 
-operacoes_validas = ('sum', 'sub', 'mul', 'div')
-
-if operacao not in operacoes_validas:
-    print('Utilize apenas as operações válidas:\nsum\nsub\nmul\ndiv')
+valid_operations = ("sum", "sub", "mul", 'div')
+if operation not in valid_operations:
+    print("Operação inválida")
+    print(valid_operations)
     sys.exit(1)
 
-numeros_validos = []
-for num in numeros:
-    if not num.isdigit():
-        print('Número inválido.')
-#print(argumentos[chave] )
+validated_nums = []
+for num in nums:
+    if not num.replace(".", "").isdigit():
+        print(f"Numero inválido {num}")
+        sys.exit(1)
+    if "." in num:
+        num = float(num)
+    else:
+        num = int(num)
+    validated_nums.append(num)
+
+n1, n2 = validated_nums
+
+if operation == "sum":
+    result = n1 + n2
+elif operation == "sub":
+    result = n1 - n2
+elif operation == "mul":
+    result = n1 * n2
+elif operation == "div":
+    result = n1 / n2
+
+print(f"O resultado é {result}")
